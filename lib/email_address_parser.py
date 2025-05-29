43e78cde9483c4ce220808b37c09e088c905b758
rxhma-sys <rahramadhan8@gmail.com>
2025-05-29 12:41:22 +0300

    done

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,12 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, email_addresses):
+        self.email_addresses = email_addresses
+
+    def parse(self):
+        # Find all email addresses using regex pattern
+        email_pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+        addresses = re.findall(email_pattern, self.email_addresses)
+        # Get unique addresses and sort alphabetically
+        return sorted(list(set(addresses)))
